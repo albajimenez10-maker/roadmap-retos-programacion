efe673b5aee0e2f11e955208f4bdcc22cc005310
albajimenez10-maker <alba.jimenez10@estudiant.upf.edu>
2025-12-15 11:29:57 +0100

    Createalba_jimenez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/alba_jimenez.py
@@ -0,0 +1,35 @@
+# https://www.python.org/
+
+# Comentario en una línea
+
+"""
+Comentario 
+en varias
+líneas
+"""
+
+'''
+Otro comentario 
+en varias
+líneas
+'''
+
+my_variable = "Mi variable"
+
+my_variable = "Mi variable con cambio"
+
+MY_CONSTANT = "Mi constante"
+
+my_int: int = 1
+my_float = 1.5 
+my_bool = True
+my_bool = False
+my_string = "Mi cadnea de texto" 
+
+print ("¡Hola, Python!") 
+
+print (type(my_int))
+print (type(my_float))
+print (type(my_bool))
+print (type(my_string))
+
